e07e38163dcc060bf488b67a3cc89fbaafe5af4d
JesminNancy <jesminnancy@hotmail.com>
2022-08-16 21:54:39 +0600

    Put method API

--- api/app/Http/Controllers/DeviceController.php
+++ api/app/Http/Controllers/DeviceController.php
@@ -22,4 +22,17 @@ class DeviceController extends Controller
 
         
     }
+
+    function update(Request $request){
+        $device= Device::find($request->id);
+        $device->name=$request->name;
+        $device->member_id=$request->member_id;
+        $result=$device->save();
+        if($result){
+            return ["result"=>"Update has been saved"];
+        }
+        else{
+            return ["result"=>"Update failed"];
+        }    
+    }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('add',[DeviceController::class,'addData']);
+Route::put('update',[DeviceController::class,'update']);
